7ee5d2c30fa4d9022371d48ff7721579d5968924
Arul Palaniappa S <118457947+arul0076@users.noreply.github.com>
2024-08-02 14:42:23 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -128,7 +128,7 @@ downloadBtn.addEventListener("click", (e) => {
     const blob = new Blob([outputText], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
-    a.download = `translated-to-${outputLanguage}.pdf`;
+    a.download = `translated-to-${outputLanguage}.txt`;
     a.href = url;
     a.click();
   }
